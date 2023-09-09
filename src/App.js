1a698456b515bb5b84f2596f244eb0787dfb0129
sergio88888123123 <137866905+sergio88888123123@users.noreply.github.com>
2023-09-08 23:08:43 -0600

    Primer avance
    Primer avance

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  const handleDateChange = (event) => {
+    setSelectedDate(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <header className="navbar"> {/* Cambio de div a header */}
+   
+        <ul className="nav-links">
+          <li><a href="https://open.spotify.com/intl-es/track/1L0C3xvOtzHSOSZ5T59n0L?si=55a42004a214479d">Inicio</a></li>
+          <li><a href="#">Existencias</a></li>
+          <li><a href="#">Catálogo</a></li>
+          <li><a href="http://localhost:3000/?producto=&seccion=&fecha=2023-10-04&fechadecaducidad=2023-09-15#">Formularios</a></li>
+          <li><a href="#">Clientes</a></li>
+        </ul>
+      </header> {/* Cierre del header */}
+
+      <div className="left-panel">
+        <div className="App">
+          <h1>Registro de Mercancía</h1>
+
+          <form>
+            <div className="form-group">
+              <label htmlFor="producto">Producto</label>
+              <input type="text" id="producto" name="producto" />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="seccion">Sección</label>
+              <input type="text" id="seccion" name="seccion" />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="fecha">Fecha de almacenaje</label>
+              <input
+                type="date"
+                id="fecha"
+                name="fecha"
+                value={selectedDate}
+                onChange={handleDateChange}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="fechacaducidad">Fecha de Caducidad</label>
+              <input
+                type="date"
+                id="fechacaducidad"
+                name="fechacaducidad"
+              />
+            </div>
+
+            <button type="submit">Enviar</button>
+          </form>
+        </div>
+      </div>
+
+      <div className="right-panel">
+        <table>
+          <thead>
+            <tr>
+              <th>Encabezado 1</th>
+              <th>Encabezado 2</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Dato 1</td>
+              <td>Dato 2</td>
+            </tr>
+            <tr>
+              <td>Dato 3</td>
+              <td>Dato 4</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
